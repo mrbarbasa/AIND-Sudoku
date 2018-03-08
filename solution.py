60fb00b36c46df3df9e6b3449d4f1681990e3368
Marifel Barbasa <mrbarbasa@gmail.com>
2018-03-08 10:02:59 -1000

    First attempt at implementing the naked twins technique

--- solution.py
+++ solution.py
@@ -5,10 +5,9 @@ from utils import *
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
-
-# TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+diagonal_units = [['A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8', 'I9'],
+                  ['A9', 'B8', 'C7', 'D6', 'E5', 'F4', 'G3', 'H2', 'I1']]
+unitlist = row_units + column_units + square_units + diagonal_units
 
 
 # Must be called after all units (including diagonals) are added to the unitlist
@@ -43,8 +42,52 @@ def naked_twins(values):
     and because it is simpler (since the reduce_puzzle function already calls this
     strategy repeatedly).
     """
-    # TODO: Implement this function!
-    raise NotImplementedError
+    # Go through every box and check for 2 remaining possibilities
+    possible_twins = [box for box in boxes if len(values[box]) == 2]
+    # unsolved_vals = [values[box] for box in possible_twins]
+
+    all_twins = []
+    processed_twins = []
+
+    for box in possible_twins:
+        # Skip the rest of the loop if the current box has already been processed
+        if box in processed_twins:
+            continue
+
+        # Eliminate the current box from the list of possible peers
+        possible_peers = [pt for pt in possible_twins if pt != box]
+
+        # Check to see if one of the other possible twins is a peer of the current box
+        #   (26 peers for diagonal boxes such as A1, 20 peers for all others)
+        peer_boxes = [peer for peer in possible_peers if peer in peers[box]]
+
+        # Check to see if one of the peer boxes is a naked twin
+        twin_boxes = [peer for peer in peer_boxes if values[peer] == values[box]]
+
+        # There could be a twin in another unit (3 boxes or more total, that form "naked twins"),
+        #   but for simplicity, let's assume that there is only one possible twin
+        twin_box = None
+        if len(twin_boxes) > 0:
+            twin_box = twin_boxes[0]
+            all_twins.append((box, twin_box))
+            processed_twins.append(box)
+            processed_twins.append(twin_box)
+
+        # Skip the rest of the loop if the current box does not have a naked twin
+        if twin_box is None:
+            continue
+
+        # Now that a pair of twins has been found, go over all the boxes in the unit(s) they
+        #   share and eliminate the twins' values from these unit peers
+        # Note: Can have 2 units if both twins are in the same 3x3 grid
+        for unit in unitlist:
+            if box in unit and twin_box in unit:
+                for peer in unit:
+                    values_to_eliminate = values[box]
+                    for digit in values_to_eliminate:
+                        values[peer] = values[peer].replace(digit, '')
+
+    return values
 
 
 def eliminate(values):
@@ -63,8 +106,13 @@ def eliminate(values):
     dict
         The values dictionary with the assigned values eliminated from peers
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for box in values:
+        digits = values[box]
+        if len(digits) == 1:
+            for peer in peers[box]:
+                if peer in values:
+                    values[peer] = values[peer].replace(digits, '')
+    return values
 
 
 def only_choice(values):
@@ -87,8 +135,16 @@ def only_choice(values):
     -----
     You should be able to complete this function by copying your code from the classroom
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for unit in unitlist:
+        for digit in '123456789':
+            # Print the box name if the current digit '1' is a possibility
+            dplaces = [box for box in unit if digit in values[box]]
+            # If the digit is in only one of the boxes,
+            #   then populate that box with the digit
+            if len(dplaces) == 1:
+                only_choice_box = dplaces[0]
+                values[only_choice_box] = digit
+    return values
 
 
 def reduce_puzzle(values):
@@ -103,10 +159,30 @@ def reduce_puzzle(values):
     -------
     dict or False
         The values dictionary after continued application of the constraint strategies
-        no longer produces any changes, or False if the puzzle is unsolvable 
+        no longer produces any changes, or False if the puzzle is unsolvable
     """
-    # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Eliminate Strategy
+        values = eliminate(values)
+
+        # Only Choice Strategy
+        values = only_choice(values)
+
+        # Naked Twins Strategy
+        values = naked_twins(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 
 def search(values):
@@ -128,8 +204,22 @@ def search(values):
     You should be able to complete this function by copying your code from the classroom
     and extending it to call the naked twins strategy.
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    # "Using depth-first search and propagation, try all possible values."
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes):
+        return values ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 
 def solve(grid):
@@ -139,7 +229,7 @@ def solve(grid):
     ----------
     grid(string)
         a string representing a sudoku grid.
-        
+
         Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
 
     Returns
@@ -153,10 +243,14 @@ def solve(grid):
 
 
 if __name__ == "__main__":
+    # Naked Twins grid
+    # diag_sudoku_grid = '84.632.....34798257..518.6...6.97..24.8256..12..84.6...8..65..3.54.2.7.8...784.96'
+    # Actual Diagonal Sudoku grid
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(grid2values(diag_sudoku_grid))
     result = solve(diag_sudoku_grid)
     display(result)
+    # print(result)
 
     try:
         import PySudoku
